de89575cf1a9923f24de2584ea2f17f429805063
Richard1669 <349966010@gapps.yrdsb.ca>
2025-05-12 09:08:14 -0400

    added code to the apple and my world

--- Apple.java
+++ Apple.java
@@ -8,12 +8,23 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Apple extends Actor
 {
-    /**
-     * Act - do whatever the Apple wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    int speed = 1;
     public void act()
     {
-        // Add your action code here.
+        int x = getX();
+        int y = getY() + 2;
+        setLocation(x, y);
+        
+        MyWorld world = (MyWorld) getWorld();
+        if(getY() >= world.getHeight())
+        {
+            world.removeObject(this);
+        }
+    }
+    
+    public void setSpeed(int spd)
+    {
+        speed = spd;
     }
 }
+
--- MyWorld.java
+++ MyWorld.java
@@ -1,8 +1,22 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+    public int score = 0;
+    int level = 1;
     public MyWorld() {
         super(600, 400, 1, false);
+        createApple();
+
+    }
+    
+    public void createApple()
+    {
+        Apple apple = new Apple();
+        apple.setSpeed(level);
+        int x = Greenfoot.getRandomNumber(600);
+        int y = 0;
+        addObject(apple, x, y);
     }
 }
+
 
